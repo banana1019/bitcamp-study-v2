375f0ff2da996b347664e5925c3efe7cb004a87b
banana1019 <jyho930@gmail.com>
2021-12-10 10:21:40 +0900

    2021-12-10

--- java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0431.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0431.java
@@ -6,6 +6,7 @@ public class Exam0431 {
   public static void main(String[] args) {
     // break
     for (int i = 1; i <= 10; i++) {
+      System.out.print("i: " + i + "   ");
       for (int j = 1; j <= i; j++) {
         System.out.print(j + " ");
         if (j == 5)
--- java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0432.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0432.java
@@ -7,15 +7,17 @@ public class Exam0432 {
     // break 라벨명;
     loop1: {
       for (int i = 1; i <= 10; i++) {
+        System.out.print("i : " + i + "  ");
         for (int j = 1; j <= i; j++) {
           System.out.print(j + " ");
           if (j == 5)
             break loop1; // 라벨로 지정한 문장을 나간다.
         }
-        System.out.println();
+        System.out.println("야호");
       }
       System.out.println("-------------------------");
     }
+    System.out.println("\nloop1 밖이다.");
   }
 }
 
