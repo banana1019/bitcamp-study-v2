94914a1b30682041dbf7199762a4d0a677454580
banana1019 <jyho930@gmail.com>
2021-12-11 22:07:44 +0900

    토요일

--- java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0430.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0430.java
@@ -6,7 +6,7 @@ public class Exam0430 {
   public static void main(String[] args) {
     // for 문의 중첩
     for (int i = 1; i <= 10; i++) {
-      System.out.println("i : " + i);
+      System.out.print("i: " + i + "   ");
       for (int j = 1; j <= i; j++) {
         System.out.print(j + " ");
       }
--- java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0433.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex06/Exam0433.java
@@ -6,6 +6,7 @@ public class Exam0433 {
   public static void main(String[] args) {
     // continue
     for (int i = 1; i <= 10; i++) {
+      System.out.print("i: " + i + "   ");
       for (int j = 1; j <= i; j++) {
         if (j % 2 == 0)
           continue; // 다음 줄로 가지 않고 '변수증가문'으로 이동한다.
