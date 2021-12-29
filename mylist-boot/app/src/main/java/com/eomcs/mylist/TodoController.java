692b8dacff48911bc669ef9fd6bbe0568ed8676a
banana1019 <jyho930@gmail.com>
2021-12-30 06:56:51 +0900

    2021-12-29

--- mylist-boot/app/src/main/java/com/eomcs/mylist/TodoController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/TodoController.java
@@ -8,47 +8,50 @@ import org.springframework.web.bind.annotation.RestController;
 // 이 표시(애노테이션)가 붙어있어야만 스프링 부트가 인식한다.
 public class TodoController {
 
+  // Todo 객체 목록을 저장할 메모리를 준비한다.
+  ArrayList todoList = new ArrayList();
+
   @RequestMapping("/todo/list") // 클라이언트 요청을 다루는 역할
   public Object list() {
-    return ArrayList2.toArray(); // 복사한 항목들을 담고 있는 새 배열을 리턴한다.
+    return ArrayList.toArray(todoList); // 복사한 항목들을 담고 있는 새 배열을 리턴한다.
   };
 
 
   @RequestMapping("/todo/add")
   public Object add(Todo todo) {
     // System.out.println(todo);
-    ArrayList2.add(todo);
-    return ArrayList2.size;
+    ArrayList.add(todoList, todo);
+    return todoList.size;
   }
 
 
   @RequestMapping("/todo/update")
   public Object update(int index, Todo todo) {
-    if (index < 0 || index >= ArrayList2.size) {
+    if (index < 0 || index >= todoList.size) {
       return 0;
     } 
 
-    return ArrayList2.set(index, todo) == null ? 0 : 1;
+    return ArrayList.set(todoList, index, todo) == null ? 0 : 1;
   }
 
 
   @RequestMapping("/todo/check")
   public Object check(int index, boolean done) {
-    if (index < 0 || index >= ArrayList2.size) {
+    if (index < 0 || index >= todoList.size) {
       return 0;  // 인덱스가 무효해서 설정하지 못했다.
     } 
-    ((Todo) ArrayList2.list[index]).done = done;
+    ((Todo) todoList.list[index]).done = done;
     return 1;  // 해당 항목의 상태를 변경했다.
   }
 
 
   @RequestMapping("/todo/delete")
   public Object delete(int index) {
-    if (index < 0 || index >= ArrayList2.size) {
+    if (index < 0 || index >= todoList.size) {
       return 0;
     } 
 
-    ArrayList2.remove(index);  // 메서드 이름으로 코드의 의미를 짐작할 수 있다. 이것이 메서드로 분리하는 이유이다.
+    ArrayList.remove(todoList, index);  // 메서드 이름으로 코드의 의미를 짐작할 수 있다. 이것이 메서드로 분리하는 이유이다.
     return 1;
   }
 }
