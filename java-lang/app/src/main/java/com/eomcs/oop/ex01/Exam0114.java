bafef64ac820d71220a89a415ccc11541229ca28
banana1019 <jyho930@gmail.com>
2022-01-02 22:57:33 +0900

    2021-12-31

--- java-lang/app/src/main/java/com/eomcs/oop/ex01/Exam0114.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex01/Exam0114.java
@@ -19,6 +19,7 @@ public class Exam0114 {
     Score s = createScore("홍길동", 100, 100, 100);
 
     printScore(s);
+
   }
 
   static void printScore(Score s) {
@@ -30,6 +31,7 @@ public class Exam0114 {
   // - Score 인스턴스를 생성하여 리턴한다.
   // - 더 정확하게 표현하면, Score 인스턴스를 생성한 후 그 주소를 리턴한다.
   static Score createScore(String name, int kor, int eng, int math) {
+
     Score s = new Score();
 
     s.name = name;
@@ -40,6 +42,7 @@ public class Exam0114 {
     s.aver = s.sum / 3;
 
     return s; // s에 저장된 인스턴스의 주소를 리턴한다.
+
     // 강사님! 로컬 변수는 메서드 호출이 끝난 다음에 삭제된다고 했는데
     // 위에서 생성한 s 변수도 삭제되고, 
     // 파라미터 name, kor, eng, math도 삭제되는게 아닌가요?
